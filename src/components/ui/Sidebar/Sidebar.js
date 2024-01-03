6302b0ea90849e0608826939ab4836d4b8d7d720
Aurnab <zubayeraurnab@gmail.com>
2024-01-03 22:48:58 +0600

    Home page created

--- src/components/ui/Sidebar/Sidebar.js
+++ src/components/ui/Sidebar/Sidebar.js
@@ -1,4 +1,3 @@
-
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -7,6 +6,10 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Image from "next/image";
 import topNews4 from "../../../assets/image_4.png";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import image5 from "../../../assets/image_5.png";
+import image6 from "../../../assets/image_6.png";
 const Sidebar = () => {
   return (
     <div className="mt-5">
@@ -41,6 +44,102 @@ const Sidebar = () => {
           </Typography>
         </CardContent>
       </Card>
+
+      <Box>
+        <Grid>
+          <Card className="mt-10" sx={{ display: "flex" }}>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <Typography component="div" variant="h5">
+                  Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  color="text.secondary"
+                  component="div"
+                >
+                  Mac Miller
+                </Typography>
+              </CardContent>
+              <Box
+                sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}
+              ></Box>
+            </Box>
+            <CardMedia>
+              <Image className="w-full h-full" src={image5} alt="side photo " />
+            </CardMedia>
+          </Card>
+          <Card className="mt-10" sx={{ display: "flex" }}>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <Typography component="div" variant="h5">
+                  Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  color="text.secondary"
+                  component="div"
+                >
+                  Mac Miller
+                </Typography>
+              </CardContent>
+              <Box
+                sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}
+              ></Box>
+            </Box>
+            <CardMedia>
+              <Image className="w-full h-full" src={image5} alt="side photo " />
+            </CardMedia>
+          </Card>
+          <Card className="mt-10" sx={{ display: "flex" }}>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <Typography component="div" variant="h5">
+                  Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  color="text.secondary"
+                  component="div"
+                >
+                  Mac Miller
+                </Typography>
+              </CardContent>
+              <Box
+                sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}
+              ></Box>
+            </Box>
+            <CardMedia>
+              <Image className="w-full h-full" src={image5} alt="side photo " />
+            </CardMedia>
+          </Card>
+          <Card className="mt-10" sx={{ display: "flex" }}>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <Typography component="div" variant="h5">
+                  Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  color="text.secondary"
+                  component="div"
+                >
+                  Mac Miller
+                </Typography>
+              </CardContent>
+              <Box
+                sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}
+              ></Box>
+            </Box>
+            <CardMedia>
+              <Image className="w-full h-full" src={image5} alt="side photo " />
+            </CardMedia>
+          </Card>
+          <Box className="flex justify-center">
+            <Image className="mt-5 " src={image6} alt="bottom image" />
+          </Box>
+        </Grid>
+      </Box>
     </div>
   );
 };
