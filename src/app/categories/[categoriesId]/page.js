6bfa0737311be745895e2772174fe8f62b4feab2
Aurnab <zubayeraurnab@gmail.com>
2024-01-04 23:06:32 +0600

    minor fix

--- src/app/categories/[categoriesId]/page.js
+++ src/app/categories/[categoriesId]/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const DynamicNewsPage = () => {
   return (
     <div>
-      <h1>Dynamic News Page</h1>
+      <h1 className="text-2xl text-red-700 font-bold">Dynamic News Page</h1>
     </div>
   );
 };
