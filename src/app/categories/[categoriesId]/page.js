b8d0881d9ebce12459dd969bfa9585c042502169
Aurnab <zubayeraurnab@gmail.com>
2024-01-06 22:45:39 +0600

    get single data on news by id Hook created

--- src/app/categories/[categoriesId]/page.js
+++ src/app/categories/[categoriesId]/page.js
@@ -7,15 +7,13 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Image from "next/image";
+import Link from "next/link";
 const DynamicNewsPage = async ({ searchParams }) => {
   // console.log(searchParams.category)
   const { data } = await getCategoryNews(searchParams.category);
   console.log(data);
   return (
     <div>
-      {/* <h1 className="text-2xl text-red-700 font-bold">
-        Page Of {searchParams.category} Total : {data.length} News
-      </h1> */}
       <Grid
         className="mt-10"
         container
@@ -24,42 +22,53 @@ const DynamicNewsPage = async ({ searchParams }) => {
       >
         {data.map((news, i) => (
           <Grid key={i} item xs={6}>
-            <Card>
-              <CardMedia sx={{
-                "& img":{
-                  width: "100%",
-                  height: "300px",
-                  objectFit:"cover"
-                }
-              }}>
-                <Image src={news.thumbnail_url} alt="Top-news" width={800} height={100} />
-              </CardMedia>
-              <CardActions>
-                <Button size="small" sx={{ background: "red" }}>
-                  {news.category}
-                </Button>
-              </CardActions>
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                  sx={{ fontWeight: "600" }}
+            <Link href={`/${news.category.toLowerCase()}/${news._id}`}>
+              <Card>
+                <CardMedia
+                  sx={{
+                    "& img": {
+                      width: "100%",
+                      height: "300px",
+                      objectFit: "cover",
+                    },
+                  }}
                 >
-                  {news.title}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  sx={{ marginTop: "20px", marginBottom: "20px" }}
-                >
-                  By {news.author.name} - {news.author.published_date}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {news.details}
-                </Typography>
-              </CardContent>
-            </Card>
+                  <Image
+                    src={news.thumbnail_url}
+                    alt="Top-news"
+                    width={800}
+                    height={100}
+                  />
+                </CardMedia>
+                <CardActions>
+                  <Button size="small" sx={{ background: "red" }}>
+                    {news.category}
+                  </Button>
+                </CardActions>
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    sx={{ fontWeight: "600" }}
+                  >
+                    {news.title}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{ marginTop: "20px", marginBottom: "20px" }}
+                  >
+                    By {news.author.name} - {news.author.published_date}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {news.details.length > 200
+                      ? news.details.slice(0, 200) + "..."
+                      : news.details}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Link>
           </Grid>
         ))}
       </Grid>
